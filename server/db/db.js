bd360ca1df8baa5f354b874329ca00b3b370fc43
brandonjyee <yee.brandon.j@gmail.com>
2019-01-21 12:57:59 -0600

    added comment about Heroku to db.js

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,8 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+// If deploying to Heroku, when you provision a Postgres DB to your app, Heroku will set a
+// DATABASE_URL config var in the environment containing the url to the database.
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
